ceb5087dce095dfbc98ebed0158b03c32a4a6145
KellarC <kellarcarson@gmail.com>
2024-01-23 19:03:33 -0600

    Initial commit

---
+++ src/main/java/com/rhodes/tapbackend/TapBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rhodes.tapbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rhodes/tapbackend/TapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhodes.tapbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
