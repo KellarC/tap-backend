74bcafc9e67962bec63c30ec81b7324f71747e70
Kellar Carson <93416556+KellarC@users.noreply.github.com>
2024-02-28 21:45:23 -0600

    Revert "fixed structure to match other endpoints"
    This reverts commit 2b314e5698e89592b28da5b4b7256416b4190cf5.

--- src/main/java/com/rhodes/tapbackend/repositories/UserRepository.java
+++ src/main/java/com/rhodes/tapbackend/repositories/UserRepository.java
@@ -9,5 +9,4 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<ApplicationUser, Integer> {
     Optional<ApplicationUser> findByUsername(String username);
-    Optional<ApplicationUser> findByEmail(String email);
 }
