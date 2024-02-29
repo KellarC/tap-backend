c0a4f3d649f415426cabcb0219181bbb34d43d00
Kellar Carson <93416556+KellarC@users.noreply.github.com>
2024-02-28 21:45:44 -0600

    Revert "made the API call"
    This reverts commit 6b17f4157ed09bc7a93e004ebd436ec232849c58.

--- src/main/java/com/rhodes/tapbackend/controllers/UserController.java
+++ src/main/java/com/rhodes/tapbackend/controllers/UserController.java
@@ -1,9 +1,6 @@
 package com.rhodes.tapbackend.controllers;
 
 import org.springframework.web.bind.annotation.*;
-import com.rhodes.tapbackend.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,22 +10,6 @@ import java.util.Optional;
 @CrossOrigin("*")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
-
     @GetMapping("/")
     public String helloUserController() { return "User access level"; }
-
-    @PostMapping("/{oldUsername}/change-username")
-    public ResponseEntity<?> changeUsername(@PathVariable String oldUsername, @RequestParam String newUsername) {
-        userService.changeUsername(oldUsername, newUsername);
-        return ResponseEntity.ok("Username changed successfully");
-    }
-
-    @PostMapping("/{username}/change-password")
-    public ResponseEntity<?> changePassword(@PathVariable String username, @RequestParam String newPassword) {
-        userService.changePassword(username, newPassword);
-        return ResponseEntity.ok("Password changed successfully");
-    }
-
 }
