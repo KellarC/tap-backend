4e883858f2f9e77054e3ee3e43eaea65013a23b1
Kellar Carson <93416556+KellarC@users.noreply.github.com>
2024-03-05 13:58:35 -0600

    fixed change-email endpoint and clean up code

--- src/main/java/com/rhodes/tapbackend/controllers/UserController.java
+++ src/main/java/com/rhodes/tapbackend/controllers/UserController.java
@@ -1,18 +1,12 @@
 package com.rhodes.tapbackend.controllers;
 
-import com.rhodes.tapbackend.models.ApplicationUser;
 import com.rhodes.tapbackend.models.ChangeUsernameDTO;
 import com.rhodes.tapbackend.models.ChangePasswordDTO;
 import com.rhodes.tapbackend.models.ChangeEmailDTO;
-import com.rhodes.tapbackend.repositories.UserRepository;
 import com.rhodes.tapbackend.services.AuthenticationService;
 import org.springframework.web.bind.annotation.*;
-import com.rhodes.tapbackend.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -20,33 +14,10 @@ import java.util.Optional;
 public class UserController {
 
     @Autowired
-    private UserService userService;
-
-    @Autowired
     private AuthenticationService authenticationService;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private ApplicationUser applicationUser;
 
     @GetMapping("/")
     public String helloUserController() { return "User access level"; }
-
-    /*
-    @PostMapping("/{oldUsername}/change-username")
-    public ResponseEntity<?> changeUsername(@PathVariable String oldUsername, @RequestParam String newUsername) {
-        userService.changeUsername(oldUsername, newUsername);
-        return ResponseEntity.ok("Username changed successfully");
-    }
-
-
-    @PostMapping("/{username}/change-password")
-    public ResponseEntity<?> changePassword(@PathVariable String username, @RequestParam String newPassword) {
-        userService.changePassword(username, newPassword);
-        return ResponseEntity.ok("Password changed successfully");
-    }*/
 
     @PostMapping("/change-username")
     public ResponseEntity<?> changeUsername(@RequestBody ChangeUsernameDTO credentials) {
@@ -60,9 +31,9 @@ public class UserController {
         return ResponseEntity.ok("Password changed successfully");
     }
 
+    @PostMapping("/change-email")
     public ResponseEntity<?> changeEmail(@RequestBody ChangeEmailDTO changeEmailDTO) {
         authenticationService.changeEmail(changeEmailDTO.getUsername(), changeEmailDTO.getNewEmail());
         return ResponseEntity.ok("Email changed successfully");
     }
-
 }
