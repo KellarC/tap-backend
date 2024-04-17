5a11f20e033bd35df98e2e72bcb35522436cff5d
Charlie Mulholland <charliemul04@gmail.com>
2024-04-16 20:27:29 -0500

    Added endpoint to get fountains within a bounded area

--- src/main/java/com/rhodes/tapbackend/repositories/FountainRepository.java
+++ src/main/java/com/rhodes/tapbackend/repositories/FountainRepository.java
@@ -2,9 +2,18 @@ package com.rhodes.tapbackend.repositories;
 
 import com.rhodes.tapbackend.models.Fountain;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FountainRepository extends JpaRepository<Fountain, Integer> {
 
+    @Query(value="SELECT * FROM fountains WHERE minLat < :y_coord AND minLon < :x_coord AND maxLat > :y_coord AND maxLon > :x_coord", nativeQuery = true)
+    List<Fountain> getFountainsInArea(@Param("minLat") float minLat,
+                                      @Param("minLon") float minLon,
+                                      @Param("maxLat") float maxLat,
+                                      @Param("maxLon") float maxLon);
 }
