98f178a38b766421d39e4bc44ae2449f4ae3428b
daryaorgil <do113272@gmail.com>
2024-04-24 12:29:34 -0500

    need to figure out the pom file

--- src/main/java/com/rhodes/tapbackend/models/ScoreboardDTO.java
+++ src/main/java/com/rhodes/tapbackend/models/ScoreboardDTO.java
@@ -1,9 +1,60 @@
-package com.rhodes.tapbackend.models;
+/*package com.rhodes.tapbackend.models;
 
 public class ScoreboardDTO {
 
+    private Long id;
     private String playerName;
     private int score;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}*/
+
+package com.rhodes.tapbackend.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Scoreboard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String playerName;
+    private int score;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getPlayerName() {
         return playerName;
--- src/main/java/com/rhodes/tapbackend/repositories/ScoreboardRepository.java
+++ src/main/java/com/rhodes/tapbackend/repositories/ScoreboardRepository.java
@@ -1,4 +1,4 @@
-package com.rhodes.tapbackend.repositories;
+/*package com.rhodes.tapbackend.repositories;
 
 import com.rhodes.tapbackend.models.ScoreboardDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +9,18 @@ public interface ScoreboardRepository extends JpaRepository<ScoreboardDTO, Long>
     List<ScoreboardDTO> findTopScoresOrderByScoreDesc(int limit);
 
     Optional<ScoreboardDTO> findByPlayerName(String playerName);
+}*/
+
+package com.rhodes.tapbackend.repositories;
+
+import com.rhodes.tapbackend.models.Scoreboard;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface ScoreboardRepository extends JpaRepository<Scoreboard, Long> {
+    List<Scoreboard> findTopScoresOrderByScoreDesc(int limit);
+
+    Optional<Scoreboard> findByPlayerName(String playerName);
 }
 
